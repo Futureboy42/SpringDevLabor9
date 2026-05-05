0a4ace22d0f3b35e394b28d27e74265151634a8c
Botond Zsigmond <zsigmond.botond.9@gmail.com>
2026-05-05 12:19:18 +0200

    setup

---
+++ src/main/java/hu/bme/aut/springdata/springdata_lab/SpringdataLabApplication.java
@@ -0,0 +1,18 @@
+package hu.bme.aut.springdata.springdata_lab;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringdataLabApplication implements CommandLineRunner {
+
+    @Override
+    public void run(String... args) {
+    }
+
+    static void main(String[] args) {
+        SpringApplication.run(SpringdataLabApplication.class, args);
+    }
+
+}
